6bf4e52688f7df35eb211202b5b44538034d365e
Prakruthimanjunath21 <152112416+Prakruthimanjunath21@users.noreply.github.com>
2026-01-07 10:45:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"message": "Learning branching!"}
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "branching" in response.json()["message"].lower()
+
